c272b3578cf98672caefd513ff2ac5efa0094879
Rob Hoelz <rob@hoelz.ro>
2017-08-10 22:34:12 -0500

    Add script to build dummy index
    Just include my own status for now

---
+++ search-ui/build-index.js
@@ -0,0 +1,17 @@
+let lunr = require('lunr');
+let process = require('process');
+let stdout = process.stdout;
+
+let index = lunr(function() {
+    this.ref('status_id');
+    this.field('author');
+    this.field('full_text');
+
+    this.add({
+        status_id: '890551057708969984',
+        author: 'hoelzro',
+        full_text: "I'm always happy to share knowledge about text processing, shell tips & tricks, Git arcana, C & Lua, and low level Linux stuff!"
+    });
+});
+stdout.write('var savedIndexData =\n');
+stdout.write(JSON.stringify(index) + ';');
