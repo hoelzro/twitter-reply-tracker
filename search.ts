a650b62686b18ec9aab6dc7fc648d53bd114fa6a
Rob Hoelz <rob@hoelz.ro>
2017-08-10 21:04:03 -0500

    Store the marker of the last search page in DynamoDB

--- search.ts
+++ search.ts
@@ -66,6 +66,24 @@ async function loadLastSinceId(db) : Promise<string> {
     });
 }
 
+function updateLatestMaxId(db, maxId) {
+    db.putItem({
+        TableName: 'reply_status_ids',
+        Item: {
+            'status_id': {
+                S: 'latest_max_id'
+            },
+            'latest_max_id': {
+                S: maxId
+            }
+        }
+    }, (err, _) => {
+        if(err) {
+            console.warn(err, err.stack);
+        }
+    });
+}
+
 async function main() {
     if('AWS_REGION' in process.env) {
         AWS.config.update({
@@ -82,25 +100,32 @@ async function main() {
         sinceId = conversationStart;
     }
 
-    while(true) {
-        let results = await performSearch(sinceId, maxId);
+    try {
+        while(true) {
+            let results = await performSearch(sinceId, maxId);
 
-        for(let status of results.statuses) {
-            if(status.in_reply_to_status_id_str == conversationStart) {
-                console.log(status.id_str);
+            for(let status of results.statuses) {
+                if(status.in_reply_to_status_id_str == conversationStart) {
+                    console.log(status.id_str);
+                }
             }
+
+            let next_results = results.search_metadata.next_results;
+            if(next_results === undefined) {
+                break;
+            }
+            let match = /max_id=(\d+)/.exec(next_results);
+
+            if(!match) {
+                throw new Error("Unable to extract max_id from " + next_results);
+            }
+            maxId = match[1];
+        }
+    } finally {
+        if(maxId != null) {
+            updateLatestMaxId(db, maxId);
         }
 
-        let next_results = results.search_metadata.next_results;
-        if(next_results === undefined) {
-            break;
-        }
-        let match = /max_id=(\d+)/.exec(next_results);
-
-        if(!match) {
-            throw new Error("Unable to extract max_id from " + next_results);
-        }
-        maxId = match[1];
     }
 }
 
