d392495ae85ee0804da7c6ca22ae93b2e8fa8328
Rob Hoelz <rob@hoelz.ro>
2017-08-11 17:52:16 -0500

    Render statuses and store them with the index
    So that we can display them very quickly in the UI

--- search-ui/build-index.ts
+++ search-ui/build-index.ts
@@ -1,5 +1,6 @@
 // vim:sts=4 sw=4
 
+import * as https from 'https';
 import * as process from 'process';
 import * as AWS from 'aws-sdk';
 
@@ -22,24 +23,49 @@ async function getTableItems(db, tableName: string) : Promise<any> {
     });
 }
 
+async function renderStatus(statusUrl) {
+    return new Promise((resolve, reject) => {
+        https.get('https://publish.twitter.com/oembed?url=' + statusUrl, (res) => {
+            // XXX handle failure
+            let body = '';
+            res.on('data', (chunk) => { body += chunk });
+            res.on('end', () => {
+                resolve(JSON.parse(body).html);
+            });
+        });
+    });
+}
+
 async function main() {
     let db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     let results = await getTableItems(db, 'reply_status_ids');
+
+    let documents = [];
+    let documentHtml = [];
+    let docId = 0;
+
+    for(let item of results.Items) {
+        if(item.status_id.S != 'latest_max_id') {
+            let statusUrl = 'https://twitter.com/' + item.author.S + '/status/' + item.status_id.S;
+            let id = docId++;
+            documents.push(renderStatus(statusUrl).then((html) => { documentHtml[id] = html; return { id: id, author: item.author.S, full_text: item.full_text.S } }));
+            documentHtml.push(null);
+        }
+    }
+
+    documents = await Promise.all(documents);
+
     let index = lunr(function() {
-        this.ref('url');
+        this.ref('id');
         this.field('author');
         this.field('full_text');
 
-        for(let item of results.Items) {
-            if(item.status_id.S != 'latest_max_id') {
-                this.add({
-                    url: 'https://twitter.com/' + item.author.S + '/status/' + item.status_id.S,
-                    author: item.author.S,
-                    full_text: item.full_text.S
-                });
-            }
+        for(let doc of documents) {
+            this.add(doc);
         }
     });
+    stdout.write('var savedHtml =\n');
+    stdout.write(JSON.stringify(documentHtml) + ';\n');
     stdout.write('var savedIndexData =\n');
     stdout.write(JSON.stringify(index) + ';');
 }
