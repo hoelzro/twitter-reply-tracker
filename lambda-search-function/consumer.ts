50004f77ae4a72acf49969f92126f81cc001a956
Rob Hoelz <rob@hoelz.ro>
2018-06-19 22:15:34 -0500

    Use trace and clarify libraries
    To clean up the call stack, and to fix up call stack entries for
    asynchronous code.  Otherwise we just see entries in the call stack
    involved in the event loop, which can make debugging errors quite
    hard

--- lambda-search-function/consumer.ts
+++ lambda-search-function/consumer.ts
@@ -18,6 +18,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+require('trace');
+require('clarify');
+
 import { loadLastSinceId, insertIntoRepliesTable, updateLatestMaxId, performSearch } from './common';
 import * as AWS from 'aws-sdk';
 
